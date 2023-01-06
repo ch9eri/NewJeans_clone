0bf5780940cf3515fbff807a2a0c50efe19862c9
임채리 <limch9eri@google.com>
2023-01-07 04:00:23 +0900

    [feat] photobooth 구현

---
+++ src/components/Photobooth.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+import CaptureImage from './CaptureImage';
+
+const Photobooth = () => {
+  return <CaptureImage />;
+};
+
+export default Photobooth;
